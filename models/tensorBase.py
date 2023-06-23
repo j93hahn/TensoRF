49a54a8459aa4d5579cc869126cb9fd17e8089d4
Joshua Ahn <jjahn@archimedes.ttic.edu>
2023-06-23 16:18:55 -0500

    pass non-normalized xyz_coords from forward method

--- models/tensorBase.py
+++ models/tensorBase.py
@@ -424,35 +424,35 @@ class TensorBase(torch.nn.Module):
             validsigma = self.feature2density(sigma_feature)
             sigma[alpha_mask] = validsigma
 
-        return sigma
+        return sigma.detach().cpu().numpy()
 
     def forward(self, rays_chunk, white_bg=True, is_train=False, ndc_ray=False, N_samples=-1):
         # sample points
         viewdirs = rays_chunk[:, 3:6]
         if ndc_ray:
-            xyz_sampled, z_vals, ray_valid = self.sample_ray_ndc(rays_chunk[:, :3], viewdirs, is_train=is_train,N_samples=N_samples)
+            _xyz_sampled, z_vals, ray_valid = self.sample_ray_ndc(rays_chunk[:, :3], viewdirs, is_train=is_train,N_samples=N_samples)
             dists = torch.cat((z_vals[:, 1:] - z_vals[:, :-1], torch.zeros_like(z_vals[:, :1])), dim=-1)
             rays_norm = torch.norm(viewdirs, dim=-1, keepdim=True)
             dists = dists * rays_norm
             viewdirs = viewdirs / rays_norm
         else:
-            xyz_sampled, z_vals, ray_valid = self.sample_ray(rays_chunk[:, :3], viewdirs, is_train=is_train,N_samples=N_samples)
+            _xyz_sampled, z_vals, ray_valid = self.sample_ray(rays_chunk[:, :3], viewdirs, is_train=is_train,N_samples=N_samples)
             dists = torch.cat((z_vals[:, 1:] - z_vals[:, :-1], torch.zeros_like(z_vals[:, :1])), dim=-1)
-        viewdirs = viewdirs.view(-1, 1, 3).expand(xyz_sampled.shape)
+        viewdirs = viewdirs.view(-1, 1, 3).expand(_xyz_sampled.shape)
 
         if self.alphaMask is not None:
-            alphas = self.alphaMask.sample_alpha(xyz_sampled[ray_valid])
+            alphas = self.alphaMask.sample_alpha(_xyz_sampled[ray_valid])
             alpha_mask = alphas > 0
             ray_invalid = ~ray_valid
             ray_invalid[ray_valid] |= (~alpha_mask)
             ray_valid = ~ray_invalid
 
 
-        sigma = torch.zeros(xyz_sampled.shape[:-1], device=xyz_sampled.device)
-        rgb = torch.zeros((*xyz_sampled.shape[:2], 3), device=xyz_sampled.device)
+        sigma = torch.zeros(_xyz_sampled.shape[:-1], device=_xyz_sampled.device)
+        rgb = torch.zeros((*_xyz_sampled.shape[:2], 3), device=_xyz_sampled.device)
 
         if ray_valid.any():
-            xyz_sampled = self.normalize_coord(xyz_sampled)
+            xyz_sampled = self.normalize_coord(_xyz_sampled)
             sigma_feature = self.compute_densityfeature(xyz_sampled[ray_valid])
 
             validsigma = self.feature2density(sigma_feature)
@@ -481,4 +481,7 @@ class TensorBase(torch.nn.Module):
             depth_map = torch.sum(weight * z_vals, -1)
             depth_map = depth_map + (1. - acc_map) * rays_chunk[..., -1]
 
-        return rgb_map, depth_map, alpha, weight, sigma, xyz_sampled
+        # pass the non-normalized xyz_sample coordinates forward instead of the normalized ones;
+        # future modules such as sample_alpha() and generating the sigma masks will need the
+        # non-normalized coordinates
+        return rgb_map, depth_map, alpha, weight, sigma, _xyz_sampled
